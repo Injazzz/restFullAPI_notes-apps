a6f7ea38cd8b0e33fd96e94bbdee9c25764d31df
injazzz <145781431+Injazzz@users.noreply.github.com>
2024-04-19 17:19:15 +0700

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: 'index.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
